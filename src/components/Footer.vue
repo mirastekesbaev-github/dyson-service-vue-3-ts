71fb0a84e583c601494ca71fed07b1a6fe74d737
Miras <mirastekesbaev@gmail.com>
2023-07-18 23:33:12 +0600

    added structure

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <p class="footer">Footer</p>
+</template>
+
+<style lang="scss" scoped>
+p.footer {
+    color: red;
+    font-size: 40px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="container">
+        <p class="header">Header</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+p.header {
+    color: red;
+    font-size: 40px;
+}
+</style>
